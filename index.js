e5efd4ca85a2267386289bd5973af8f556df59a5
alej-mocse1 <alejandromcse01@gmail.com>
2023-06-18 23:37:08 -0300

    solucionando el ultimo problema con el despliegue

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const { conn } = require("./src/db.js");
-const server = require('./src/server.js')
+const server = require('./src/server.js');
+require('dotenv').config()
 
-const PORT = 3004
+const PORT = process.env.PORT || 3004
 
 //ejecutamos todo nuestro proyecto 
+
 server.listen(PORT,() => {
     console.log("servidor escuchando en " + PORT)
 })
