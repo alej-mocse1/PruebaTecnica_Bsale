43f9ab22e0c5936d21eac1ccdb6b7da05042ef77
alej-mocse1 <alejandromcse01@gmail.com>
2023-06-18 23:08:57 -0300

    nuevo puerto

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { conn } = require("./src/db.js");
 const server = require('./src/server.js')
 
-const PORT = 3006
+const PORT = 3004
 
 //ejecutamos todo nuestro proyecto 
 server.listen(PORT,() => {
