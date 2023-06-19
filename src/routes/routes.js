62bb7cd905b1992218dc4e00765e3a6a08f38fd9
alej-mocse1 <alejandromcse01@gmail.com>
2023-06-18 21:15:13 -0300

    arreglo para el deploy en (src/routes/routes.js)

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,7 +2,7 @@ const Router = require("express");
 
 //importamos el obj controller con todas sus props , de esta manera cuando nuestro proyecto necesite mas rutas
 //solo llamamos a la prop correspondiente sin necesidad de importar todos los controllers (de esta menera tenemos un codigo mas limpio)
-const Controller = require("../controller/controller");
+const Controller = require("../controller/Controller");
 
 
 const Route = Router();
